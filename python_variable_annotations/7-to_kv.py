0409c8060588857043564bf3569ef72a269cf54f
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-03 19:05:20 +0000

    done

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""tuple"""
+
+from typing import Union, Tuple
+
+def tp_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns tuple and string"""
+    return (k, float(v ** 2))
