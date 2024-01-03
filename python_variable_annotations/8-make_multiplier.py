2688fa2667dfed54237e331054eb3878989bc673
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-03 19:10:31 +0000

    done

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""making multiplier"""
+
+def make_multiplier(multiplier: float):
+    def inner_function(number: float) -> float:
+        return number * multiplier
+    
+    return inner_function
