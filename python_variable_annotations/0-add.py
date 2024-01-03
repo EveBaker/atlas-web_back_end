cd051b561d73ed201277f41355bfa2eb2820f149
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-03 17:29:19 +0000

    done

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Module for Python
+"""
+def add(a: float, b: float) ->float:
+    return a + b
+
+
+"""
+float: float is a built-in data type to represent real numbers.
+Mainly used with numbers that have or might have decimal parts in python.
+"""
