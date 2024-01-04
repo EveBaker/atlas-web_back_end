67480b4e2dc1ccfbbbda1546c90e9c70cbda8b99
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-04 17:56:07 +0000

    done

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""more async"""
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """float time random"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    completed_tasks = await asyncio.gather(*tasks)
+    return completed_tasks
