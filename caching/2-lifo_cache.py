4a4b5300a1e686fe04867943033581fa0b88333c
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-09 20:46:10 +0000

    done

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    BaseCache module
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO caching system that discards
+    the last item added when full."""
+
+    def __int__(self):
+        """Initalizes LIFOCache"""
+        super().__int__()
+        self_last_key_added = None
+
+        def put(self, key, item):
+            """
+            Saves an item in the cache.
+            Discards the last added item if the limit is exceeded.
+            """
+            if key is not None and item is not None:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    if self_last_key_added:
+                        del self.cache_data[self_last_key_added]
+                        print("DISCARD:", self_last_key_added)
+                        self.cache_data[key] = item
+                        self_last_key_added = key
+
+        def get(self, key):
+            """Retrieves item by key"""
+            return self.cache_data.get(key, None)
