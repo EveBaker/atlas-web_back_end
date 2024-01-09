21ddb3284b59529bbb8da25fbef182e9b91f44e6
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-09 20:03:59 +0000

    done

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Module for BasicCache class """
+
+
+from base_caching import BaseCaching
+
+
+class BaseCaching(BaseCaching):
+    """A simple caching system. It stores
+    data in a dictionary with no limit."""
+
+
+def put(self, key, item):
+    """
+    Save an item to the cache
+    If the key or item is None, nothing happens.
+    """
+    if key is not None and item is not none:
+        self.cache_data[key] = item
+
+
+def get(self, key):
+    """
+    Get an item by its key.
+    Returns None if the key is none or doesn't exist.
+    """
+    return self.cache_data.get(key, None)
