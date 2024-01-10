d33489371986884ff3ca80a280c3d7eb5edf0ea7
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-10 21:49:49 +0000

    done

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,9 +40,6 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """  
-        Get the hyper index, returns hyper index
-        """
         assert index is not None and 0 <= index < len(self.__indexed_dataset)
         assert isinstance(page_size, int) and page_size > 0
 
