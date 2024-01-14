9c5d0db0f3d80ae7edb50f70684451ed819ea1e0
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-14 23:17:51 +0000

    test

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 from typing import List
 import logging
 import re
+import os
+import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -42,3 +44,17 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db():
+    db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    db_password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhsot')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(
+        user=db_user
+        password=db_password
+        host=db_host
+        database=db_name
+    )
