e90aa300d9ee6600d596b3f606a51c61f236a86d
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-14 23:44:47 +0000

    done

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""encrypt passwords"""
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password with salt using bcrypt"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
