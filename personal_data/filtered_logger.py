ca92189b6295d92ed2baf560ef0e91f39bbafbe1
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-14 22:37:47 +0000

    done?

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]+", f"{field}={redaction}", message)
+    return message
