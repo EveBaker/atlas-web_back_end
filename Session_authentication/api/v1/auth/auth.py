0e200136ccccc615e123689bc8087a268218927b
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-22 22:32:02 +0000

    test

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ manage the API authentification
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -31,3 +32,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Gets user from request"""
         return None
+
+    def session_auth(self, request=None):
+        """retrieve the value of a cookie from Flask"""
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
