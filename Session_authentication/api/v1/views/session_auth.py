171f649300894c1256634b95c6b66541f722321f
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-22 23:08:16 +0000

    test

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -13,19 +13,15 @@ def login():
     email = request.form.get('email')
     if not email:
         return jsonify({"error": "email missing"}), 400
-        
     password = request.form.get("password")
     if not password:
         return jsonify({"error": "password missing"}), 400
-        
     user = User.search({'email': email})
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
-        
     user = user[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-        
     session_id = auth.create_session(user.id)
     session_name = os.getenv('SESSION_NAME')
     response = jsonify(user.to_json())
