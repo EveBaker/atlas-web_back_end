514a8c4e29f6ec6d05a2ecf9e61d75384f3e1395
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-22 23:03:47 +0000

    test

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" sesion auth views
+"""
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+from flask import abort, jsonify, request
+from models.user import User
+import uuid
+import os
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+        
+    password = request.form.get("password")
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+        
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+        
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+        
+    session_id = auth.create_session(user.id)
+    session_name = os.getenv('SESSION_NAME')
+    response = jsonify(user.to_json())
+    response.set_cookie(session_name, session_id)
+    return response
