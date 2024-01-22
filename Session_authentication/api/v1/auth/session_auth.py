bf3618426fbc930b2daf7af58716933f990afa32
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-22 22:45:27 +0000

    fixed pycode

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -25,7 +25,7 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None):
         """retrieve the current user associated with a session"""
         session_id = self.session_cookie(request)
