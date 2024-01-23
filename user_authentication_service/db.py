c84bed04420fb287dbf83cecac1262381dfa845b
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-23 22:24:10 +0000

    test

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
+from typing import TypeVar
 
 
 class DB:
@@ -29,8 +30,8 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds user to database"""
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """adds user"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
