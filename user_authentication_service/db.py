336fc22560a499f0d168c31fedba78f5d4731442
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-23 23:40:05 +0000

    test

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -34,10 +34,11 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds user to database"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """finds user by keyward """
