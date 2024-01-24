7c808280f3c2bf75b3b8fe0959eaebb419c90fa5
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-24 00:40:59 +0000

    test

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -27,16 +27,16 @@ class Auth:
         return user
 
 
-def valid_login(self, email: str, password: str) -> bool:
-    """Check if the provided credentials are valid."""
-    if email is None or password is None:
-        return False
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if the provided credentials are valid."""
+        if email is None or password is None:
+         return False
 
-    try:
-        user = self._db.find_user_by(email=email)
-        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
-    except NoResultFound:
-        return False
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
 
 
 def _hash_password(password: str) -> bytes:
