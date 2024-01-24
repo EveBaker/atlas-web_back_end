e6df5fcb01d9803d3e163a55d70156a600cc57b2
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-24 00:33:14 +0000

    test

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,6 +2,7 @@
 """ Flask module """
 from flask import Flask, jsonify, request
 from auth import Auth
+import bcrypt
 
 AUTH = Auth()
 app = Flask(__name__)
@@ -24,6 +25,15 @@ def register():
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+def valid_login(self, email: str, password: str) -> bool:
+    """Check if the provided credentials are valid."""
+    user = self._db.find_user_by(email=email)
+    if user is None:
+        return False
+
+    return bcrypt.checkpw(password.encode(), user.hashed_password)
 
 
 if __name__ == "__main__":
