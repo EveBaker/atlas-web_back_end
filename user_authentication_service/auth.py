4a5b1b0c5aae2c2baafb5412b4e9e1274a6de6e3
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-24 00:44:24 +0000

    test

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -26,15 +26,15 @@ class Auth:
         user = self._db.add_user(email, hashed_password)
         return user
 
-
     def valid_login(self, email: str, password: str) -> bool:
         """Check if the provided credentials are valid."""
         if email is None or password is None:
-         return False
+            return False
 
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(password.encode
+                                  ('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
 
