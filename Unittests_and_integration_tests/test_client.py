9c82af8b202725dc33f620cda20ba4ffe2c059bd
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-29 20:21:57 +0000

    test

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Test client """
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test GithubOrgClient class"""
+    @parameterized.expand([
+        ('google',),
+        ('abc',)
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test GithubOrgClient Return"""
+        endpoint = f'https://api.github.com/orgs/{org_name}'
+        github_org_client = GithubOrgClient(org_name)
+        github_org_client.org()
+        mock_get_json.assert_called_once_with(endpoint)
+
+if __name__ == "__main__":
+    unittest.main()
