27f7cd9cabd3758e50ff416f82351cf5effd014b
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-29 19:34:58 +0000

    test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -28,23 +28,23 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
-    class TestGetJson(unittest.TestCase):
-        @parameterized.expand([
-            ("http://example.com", {"payload": True}),
-            ("http://holberton.io", {"payload": False}),
-        ])
-        def test_get_json(self, test_url, test_payload):
-            """test get_json"""
-            class Mocked(Mock):
-                """mock json"""
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """test get_json"""
+        class Mocked(Mock):
+            """mock json"""
             def json(self):
                 """mocked json"""
                 return test_payload
             
-            with patch('requests.get') as MockClass:
-                MockClass.return_value = Mocked()
-                result = get_json(test_url)
-                self.assertEqual(result, test_payload)
+        with patch('requests.get') as MockClass:
+            MockClass.return_value = Mocked()
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
 
     if __name__ == "__main__":
         unittest.main()
