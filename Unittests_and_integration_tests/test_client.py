cfd07369c81623c52644d958b495b8200cf37ead
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-29 20:28:28 +0000

    test

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,6 +3,7 @@
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -18,6 +19,19 @@ class TestGithubOrgClient(unittest.TestCase):
         github_org_client = GithubOrgClient(org_name)
         github_org_client.org()
         mock_get_json.assert_called_once_with(endpoint)
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test GithubOrgClient class"""
+
+    def test_public_repos_url(self):
+        """Test GithubOrgClient._public_repos_url"""
+        mock_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        org_name = "google" 
+
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = mock_payload
+            github_org_client = GithubOrgClient(org_name)
+            self.assertEqual(github_org_client._public_repos_url, mock_payload["repos_url"])
 
 if __name__ == "__main__":
     unittest.main()
