6c0b834efafebf98f03d69bde1b6a6ede63857b0
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-29 20:54:23 +0000

    fixed pycode

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -6,6 +6,7 @@ from parameterized import parameterized
 from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
 
+
 class TestGithubOrgClient(unittest.TestCase):
     """Test GithubOrgClient class"""
     @parameterized.expand([
@@ -22,13 +23,16 @@ class TestGithubOrgClient(unittest.TestCase):
 
     def test_public_repos_url(self):
         """Test GithubOrgClient._public_repos_url"""
-        mock_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
-        org_name = "google" 
+        mock_payload = {"repos_url":
+                        "https://api.github.com/orgs/google/repos"}
+        org_name = "google"
 
-        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
             mock_org.return_value = mock_payload
             github_org_client = GithubOrgClient(org_name)
-            self.assertEqual(github_org_client._public_repos_url, mock_payload["repos_url"])
+            self.assertEqual(github_org_client._public_repos_url,
+                             mock_payload["repos_url"])
 
     @patch('client.get_json')
     def test_public_repos(self, mocked_method):
@@ -37,13 +41,15 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_method.return_value = mock_payload
         mock_repos_url = "https://api.github.com/orgs/test/repos"
 
-        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mocked_public:
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mocked_public:
             mocked_public.return_value = mock_repos_url
             response = GithubOrgClient('test').public_repos()
 
             self.assertEqual(response, ["Google", "TT"])
             mocked_public.assert_called_once()
-            mocked_method.assert_called_once_with(mock_repos_url)  
-                                     
+            mocked_method.assert_called_once_with(mock_repos_url)
+
+
 if __name__ == "__main__":
     unittest.main()
