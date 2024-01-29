8bd31c9575eb847def0431c851a41d62658afc09
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-29 20:13:23 +0000

    test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -45,6 +45,29 @@ class TestGetJson(unittest.TestCase):
             MockClass.return_value = Mocked()
             result = get_json(test_url)
             self.assertEqual(result, test_payload)
+        
+class TestMemoize(unittest.TestCase):
+    """ Test Class to memoize """
+
+    def test_memoize(self):
+        """ Test memoize """
+        class TestClass:
+            """ Test Class """
+
+            def a_method(self):
+                """ A method """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ Decorator """
+                return self.a_method()
+
+        test_class = TestClass()
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mock.assert_called_once()
 
     if __name__ == "__main__":
         unittest.main()
