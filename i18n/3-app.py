86139009284cc7e8c5fc90e33ee6951e7c7b964f
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-30 23:45:01 +0000

    test

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -25,7 +25,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """return template"""
     return render_template('3-index.html', gettext=gettext)
