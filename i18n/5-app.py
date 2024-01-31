4b3c62d7ac868aaa00172d46afcaec9bedb97077
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-31 00:16:52 +0000

    test

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -2,7 +2,7 @@
 """Flask app"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, g
+from flask_babel import Babel, gettext, g
 
 
 app = Flask(__name__)
@@ -27,14 +27,10 @@ babel = Babel(app)
 
 
 @babel.localeselector
-@babel.localeselector
 def get_locale():
-    """return languages"""
-    locale = request.args.get('locale')
-    if locale:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
+    """return language"""
+    return request.args.get('locale', request.accept_languages
+                            .best_match(app.config['LANGUAGES']))
 
 
 def get_user():
