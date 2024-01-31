47e5f403785ec7517fe73aa82e745e8ac8e3a5a7
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-31 00:29:28 +0000

    test

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -2,7 +2,7 @@
 """Flask app"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -28,7 +28,7 @@ def get_locale():
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """return template"""
-    return render_template('3-index.html', gettext=gettext)
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
