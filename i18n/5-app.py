3d5f73c8b28bcf46302a2ae7420030588bbfab7e
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-31 00:03:20 +0000

    test

---
+++ i18n/5-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Flask app"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """returns user"""
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """before request"""
+    g.user = get_user()
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """return template"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
