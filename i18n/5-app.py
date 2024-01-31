817103684143828c19610441c3c10c1aa6636f79
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-01-31 00:32:13 +0000

    test

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -8,12 +8,28 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
+class Config:
+    ''' App config '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
+
+@babel.localeselector
+def get_locale():
+    """return language"""
+    return request.args.get('locale', request.accept_languages
+                            .best_match(app.config['LANGUAGES']))
 
 
 def get_user():
