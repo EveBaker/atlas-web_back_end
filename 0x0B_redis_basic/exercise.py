e3b9cdf2f1aad24340ec5eb6341f9abdb69f1f55
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-07 23:09:39 +0000

    test

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,7 +5,7 @@ Module for Cache class.
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     """Cache class for redis storage"""
@@ -20,3 +20,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(name=key, value=data)
         return key
+
+    def get(self, key: str, fn: Callable = None):
+        """stores cache"""
+        value = self._redis.get(key)
+        if value is not None and fn:
+            return fn(value)
+        return value
+    
+    def get_str(self, key: str) -> str:
+        """gets str"""
+        return self._redit.get(key).decode("utf-8") 
+    
+    def get_int(self, key:str) -> int:
+        """Parametrized get int"""
+        return self.get(key, fn=int)
