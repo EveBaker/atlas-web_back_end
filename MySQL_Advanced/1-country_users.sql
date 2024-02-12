3bb2f68170f4e452e575f658f6c7aed99d09b14a
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-12 05:38:51 +0000

    test

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,9 @@
 -- adds country column
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(225) NOT NULL UNIQUE,
-    name VARCHAR(225),
-    country EMUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    id INT AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY (id)
 );
