01a71afe6786592310affbf69daba6b23252e6dc
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-12 05:28:18 +0000

    work in prog

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- create user
 
-Create TABLE IF NOT EXISTS users {
+Create TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT,
     email VARCHAR(225) NOT NULL UNIQUE,
     name VARCHAR (255),
     PRIMARY KEY (id)
-    };
+);
