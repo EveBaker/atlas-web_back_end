9bfddf13b3bbb381a326bafd715ab73b01f0bc3b
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-12 05:34:49 +0000

    work in prog

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- adds country column
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    email VARCHAR(225) NOT NULL UNIQUE,
+    name VARCHAR(225),
+    country EMUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+)
