6a6e3d823291cb54299c0704b673f518bd90cc11
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-12 05:37:08 +0000

    fixed typo

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- adds country column
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(225) NOT NULL UNIQUE,
     name VARCHAR(225),
     country EMUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
