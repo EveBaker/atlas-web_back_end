20547d7d1487c47ba92afd906931f82ae5ef4660
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-14 05:19:15 +0000

    work in prog

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,12 @@
+-- creates a stored procedure ComputeAverageScoreForUser
+
+DELIMITER $$
+
+CREATE procedure ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+    WHERE id = user_id;
+END$$
+
+DELIMITER ;
