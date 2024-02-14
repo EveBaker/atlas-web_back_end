080b55d8d8bbf66d4a998721286159d4220bc1c0
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-14 05:25:26 +0000

    test

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -6,9 +6,9 @@ CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
     
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-    
     SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
 END$$
 
 DELIMITER ;
