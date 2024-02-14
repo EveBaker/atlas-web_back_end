c23f3419df9fc2e9248672c09e49e185f3e62f71
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-14 05:00:36 +0000

    test

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- creates a trigger that resets the attribute valid_email
+
+DELIMITER $$
+
+CREATE TRIGGER before_email_update
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN 
+    IF OLD.email <> NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER ;
