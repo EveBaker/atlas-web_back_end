e5df620d52093b29b481659438da30f998e62b69
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-24 19:22:01 +0000

    work in prog

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString().trim()}`);
-    process.exit();
-});
+    const name = data.toString().trim();
+    console.log(`Your name is: ${name}`);
 
-process.on('exit', () => {
     console.log('This important software is now closing');
-});
+    process.exit();
+  });
