c242c060aec0256445335be77e394a1f4f5a3a34
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-26 15:36:20 +0000

    test

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,39 +1,39 @@
 const fs = require('fs');
 
 function countStudents(path) {
-try {
-    //readfile
+  try {
+    // Read the file
     const data = fs.readFileSync(path, 'utf8');
 
-    //splits into lines
+    // Split the file 
     const lines = data.split('\n').filter(line => line !== '');
 
-    //remove header
+    // Remove the header
     lines.shift();
 
     const students = lines.map(line => {
-        const [firstName, , field] = line.split(',');
-        return { firstName, field};
+      const [firstName, , field] = line.split(',');
+      return { firstName, field };
     });
 
     const count = students.length;
     console.log(`Number of students: ${count}`);
 
-    // count by field
+    // Count students by field
     const fields = {};
     students.forEach(student => {
-        if (!fields[student.field]) {
-            fields[student.field] = [];
-        }
-        fields[student.field].push(student.firstName);
+      if (!fields[student.field]) {
+        fields[student.field] = [];
+      }
+      fields[student.field].push(student.firstName);
     });
 
     for (const field in fields) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
     }
-} catch (err) {
+  } catch (err) {
     throw new Error('Cannot load the database');
-}
+  }
 }
 
 module.exports = countStudents;
