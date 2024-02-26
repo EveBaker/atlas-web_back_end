68ee1aebeb5f0d7f5c80476398e5c74123f4a13e
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-26 17:00:28 +0000

    lint fixed

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = 12345;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running and listening on port ${PORT}`);
+  console.log(`Server is running and listening on port ${PORT}`);
 });
 
 module.exports = app;
