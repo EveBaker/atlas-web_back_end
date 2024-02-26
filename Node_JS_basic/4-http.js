fc941e34fcdb339b95a213d662d0f57b695d7b4d
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-26 16:13:41 +0000

    node is correct

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,18 +1,19 @@
 const http = require('http');
 
-//create server
+// Create server
 const app = http.createServer((req, res) => {
-//set
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-//send
-res.end('Hello Holberton School!\n');
+  // Set headers
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+  // Send
+  res.end('Hello Holberton School!\n');
 });
 
-//listen on port
+// Listen on port 
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server is running and listening on port ${PORT}`);
+  console.log(`Server is running and listening on port ${PORT}`);
 });
 
+// Export the server
 module.exports = app;
