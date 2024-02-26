cbc6fe438e0b24aa8f20f3ad90d91d7bf44d70a2
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-26 15:34:33 +0000

    test

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,37 +1,39 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    let content;
-}
-
 try {
-    content = fs.readFileSync(path, 'utf8');
-    
+    //readfile
+    const data = fs.readFileSync(path, 'utf8');
+
+    //splits into lines
+    const lines = data.split('\n').filter(line => line !== '');
+
+    //remove header
+    lines.shift();
+
+    const students = lines.map(line => {
+        const [firstName, , field] = line.split(',');
+        return { firstName, field};
+    });
+
+    const count = students.length;
+    console.log(`Number of students: ${count}`);
+
+    // count by field
+    const fields = {};
+    students.forEach(student => {
+        if (!fields[student.field]) {
+            fields[student.field] = [];
+        }
+        fields[student.field].push(student.firstName);
+    });
+
+    for (const field in fields) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
 } catch (err) {
     throw new Error('Cannot load the database');
 }
-
-//split content, filter empty lines
-const lines = content.split('\n').filter(line => line);
-
-//number of students
-const NUMBER_OF_STUDENTS = lines.length - 1;
-console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
-
-//initalize
-const fields = {};
-
-for (let i = 1; i < lines.length; i++) {
-    const [name, , , field] = lines[i].split(',');
-    if (field) { // Check if field is not undefined
-      if (!fields[field]) fields[field] = [];
-      fields[field].push(name);
-    }
-  }
-
-//Itorate student log info
-for (const [key, value] of Object.entries(fields)) {
-    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
-  }
+}
 
 module.exports = countStudents;
