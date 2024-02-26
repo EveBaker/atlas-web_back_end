810587c3b952e8dec0cbe8066464fac26d8ebd10
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-26 17:24:16 +0000

    trying for more checks

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -25,7 +25,8 @@ function countStudents(path) {
       });
 
       Object.keys(fields).forEach((field) => {
-        console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      });
     })
     .catch(() => {
       throw new Error('Cannot load the database');
