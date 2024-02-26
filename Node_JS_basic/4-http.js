94bec01672c69613b940480bcb445f6e453020ca
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-26 16:16:14 +0000

    fixed lint

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,12 +4,12 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   // Set headers
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  
+
   // Send
   res.end('Hello Holberton School!\n');
 });
 
-// Listen on port 
+// Listen on port
 const PORT = 1245;
 app.listen(PORT, () => {
   console.log(`Server is running and listening on port ${PORT}`);
