df1545b33efcc2be1a10b28e46a03c0828930036
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-26 16:34:13 +0000

    test

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -30,7 +30,6 @@ const app = http.createServer(async (req, res) => {
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
