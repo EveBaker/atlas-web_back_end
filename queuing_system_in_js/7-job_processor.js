1bb7e87de57312e8afc97f6029a6e10afae69697
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-02-27 17:31:02 +0000

    tests passing

---
+++ queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,25 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+// Array of blacklisted phone numbers
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+function sendNotification(phoneNumber, message, job, done) {
+    job.progress(0, 100);
+
+    if (blacklistedNumbers.includes(phoneNumber)) {
+        return done (new Error(`Phone number ${phoneNumber} is blacklisted`));
+    }
+
+    // Simulate progress
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+
+    // Simulate completion
+    done();
+}
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message, job, done);
+  });
